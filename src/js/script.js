66796d74838ae10517319b542c0785c041c4a617
Tetiana <ostannja@gmail.com>
2021-03-15 13:34:45 +0200

    info popups

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,46 @@
+"use strict";
+
+var popups = document.querySelectorAll(".info_icon");
+Array.prototype.forEach.call(popups, function (el) {
+  el.onclick = function () {
+    this.nextElementSibling.style.display = "block";
+  };
+});
+
+var close = document.querySelectorAll(".close_popup");
+
+Array.prototype.forEach.call(close, function (cl) {
+  cl.onclick = function () {
+    this.parentElement.style.display = "none";
+  };
+});
+
 document.getElementById("zoom").addEventListener("click", function () {
   document.querySelector(".zoom_bg").style.display = "block";
 });
 document.querySelector(".close").addEventListener("click", function () {
   document.querySelector(".zoom_bg").style.display = "none";
 });
+
+document.addEventListener("click", function (event) {
+  var e = document.querySelector("modal_content");
+  if (!e.contains(event.target)) e.style.display = "none";
+});
+
+document.querySelector("info").addEventListener("click", function () {
+  document.querySelector(".report_popup").style.display = "block";
+});
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".zoom_bg").style.display = "none";
+});
+
+// const popup = document.querySelector(".popup");
+// function showPopup() {
+//   popup.classList.add("open");
+// }
+// function hidePopup() {
+//   popup.classList.remove("open");
+// }
 
 function testWebP(callback) {
   var webP = new Image();
